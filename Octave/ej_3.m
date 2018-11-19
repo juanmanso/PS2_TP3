68987e9c4d03e39e8a844ca349d778cde8258573
Emiliano Gasparovic <emilianit2000@gmail.com>
2018-11-19 19:37:35 -0300

    Corregido signo vector de entrada_ventaneada

--- Octave/ej_3.m
+++ Octave/ej_3.m
@@ -1,6 +1,6 @@
 config_m;
 
-largo_senial_entrada = 50000;     % Es el largo de la senial u de referencia.
+largo_senial_entrada = 4000;     % Es el largo de la senial u de referencia.
 largo_parte_fir = 2;              % Cantidad de coeficientes del numerador.
 largo_parte_iir = 2;              % Cantidad de coeficientes del denominador.
 mu = 0.1;                         % Constante de aprendizaje.
@@ -26,7 +26,7 @@ w = zeros(largo_parte_fir + largo_parte_iir, length(entrada) - largo_parte_fir);
 
 % Algoritmo LMS.
 for i = (largo_parte_fir + largo_parte_iir) : length(entrada)
-    entrada_ventaneada = [entrada(i : -1 : i - largo_parte_fir + 1), salida(i - 1 : -1 : i - largo_parte_iir)];
+    entrada_ventaneada = [entrada(i : -1 : i - largo_parte_fir + 1), -salida(i - 1 : -1 : i - largo_parte_iir)];
     w(:, i) = w(:, i - 1) + mu * entrada_ventaneada' * (salida(i) - entrada_ventaneada * w(:, i - 1));
 end
 
@@ -37,12 +37,24 @@ w(:, end)
 figure(2);
 hold on;
 plot(w');
-axis([largo_parte_fir + largo_parte_iir, largo_senial_entrada, -0.3, 0.7]);
+fir_line_1=refline(0,B(1));
+fir_line_2=refline(0,B(2));
+fir_line_3=refline(0,A(2));
+fir_line_4=refline(0,A(3));
+fir_line_1.LineStyle='--';
+fir_line_1.LineWidth=2;
+fir_line_2.LineStyle='--';
+fir_line_2.LineWidth=2;
+fir_line_3.LineStyle='--';
+fir_line_3.LineWidth=2;
+fir_line_4.LineStyle='--';
+fir_line_4.LineWidth=2;
+%axis([largo_parte_fir + largo_parte_iir, largo_senial_entrada, -0.3, 0.7]);
 title('Convergencia De Coeficientes');
 
 % Obtención de los resultados
 
-result=solve(w(1:2,end)'==numB,-w(3:4,end)'==denA(2:3),[m k b]);
+result=solve(w(1:2,end)'==numB,w(3:4,end)'==denA(2:3),[m k b]);
 result=double(struct2array(result));
 fprintf('Los resultados de la estimación por LMS son:\n')
 fprintf('m=%.2f \nk=%.2f \nb=%.2f\n',result(1),result(2),result(3))
